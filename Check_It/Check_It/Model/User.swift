f260196d3e9d387554481ed6d8f0fd4a9199a2d4
김응관 <kwhn9486@naver.com>
2023-01-05 20:41:22 +0900

    [Add] User 모델뷰 추가

---
+++ Check_It/Check_It/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Check_It
+//
+//  Created by 최한호 on 2023/01/05.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    var userId: UUID().uuidString
+    var userImg: String
+    var userName:
+}
