2e2c469a11171f555c0b5cfe65c9c5790a0b812e
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-06 02:45:52 +0900

    [Feat] MapKit과 Qrcode연동 기능 구현

--- Check_It/Check_It/View/Home/MapView.swift
+++ Check_It/Check_It/View/Home/MapView.swift
@@ -11,9 +11,32 @@ import MapKit
 struct MapView: View {
     @EnvironmentObject var locationViewModel: LocationViewModel
     
+    @State private var isQrcode: Bool = false
+    
     var body: some View {
         VStack {
             Map(coordinateRegion: $locationViewModel.region, showsUserLocation: true)
+                .frame(minWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height * 0.7)
+            Spacer()
+        }
+        .edgesIgnoringSafeArea([.top, .leading, .trailing])
+        
+        .toolbar {
+            ToolbarItem {
+                Button(action: {
+                    isQrcode.toggle()
+                    print("큐알코드 클릭함")
+                }, label: {
+                    Image(systemName: "qrcode.viewfinder")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+                })
+                .sheet(isPresented: $isQrcode) {
+                    QRView()
+                        .presentationDetents([.medium])
+                }
+            }
         }
     }
 }
@@ -21,5 +44,6 @@ struct MapView: View {
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
+            .environmentObject(LocationViewModel())
     }
 }
