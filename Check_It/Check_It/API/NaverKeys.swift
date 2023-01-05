c2e600d9966d7384657f4be2a77defcf335d6600
Hyemin Heo <heohm128@gmail.com>
2023-01-05 21:50:00 +0900

    add: add packages

---
+++ Check_It/Check_It/API/NaverKeys.swift
@@ -0,0 +1,10 @@
+//
+//  NaverKeys.swift
+//  Check_It
+//
+//  Created by sole on 2023/01/05.
+//
+
+import Foundation
+let naverClientId: String = "653hfg2t7w"
+let naverClientSecretKey: String = "p82MjTygQ4l3okUqMkII6xkMIzvYE4msD19Gtm1e"
