00f7c53109f20cbee9cf5058fc9716acb677610a
김응관 <kwhn9486@naver.com>
2023-01-05 09:48:39 +0900

    [Add] 프로젝트 생성

---
+++ Check_It/Check_It/Check_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_ItApp.swift
+//  Check_It
+//
+//  Created by 김응관 on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct Check_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
