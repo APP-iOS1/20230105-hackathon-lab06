10c7b99b9cf5546731fddac5f80d7cb70be454ea
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-06 06:06:06 +0900

    [Feat] 약속 장소와 유저사이의 거리를 보여주는 기능 구현

--- Check_It/Check_It/View/Home/MapView.swift
+++ Check_It/Check_It/View/Home/MapView.swift
@@ -11,16 +11,38 @@ import MapKit
 struct MapView: View {
     @EnvironmentObject var locationViewModel: LocationViewModel
     
-    @State private var isQrcode: Bool = false
+    @State private var isQrcode: Bool = false // 바코드 유무
     
-    var sampleLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 37.478846, longitude: 126.620930)
+    var promisse: Promise = Promise(promiseName:,
+                                    limit: <#T##String#>,
+                                    lateLimit: <#T##String#>,
+                                    date: <#T##String#>,
+                                    startTime: <#T##String#>,
+                                    endTime: <#T##String#>)
     
-    var distance: Double {
-        locationViewModel.calcDistance(
+    // 임시로 만든 위치(실제로는 모임장소의 위도 경도가 들어감)
+    var sampleLocation: CLLocationCoordinate2D =  CLLocationCoordinate2D(latitude: 37.478846, longitude: 126.620930)
+    
+    var sampleLimitDis: Int = 10 // 임시로 만든 제한 거리 단위는 m
+    
+    var distance: Int { // 두 지점사이의 거리를 나타냄
+        let dis = locationViewModel.calcDistance(
             lan1: sampleLocation.latitude,
             lng1: sampleLocation.longitude,
             lan2: locationViewModel.region.center.latitude,
             lng2: locationViewModel.region.center.longitude)
+        return dis
+    }
+    
+    var stringsDis: String {
+        if distance > 1 { // 1보다 크면 km단위
+            let strings = String(distance)
+            let distanceStr = strings.components(separatedBy: ".")
+            let km = distanceStr.first ?? "0"
+            return "\(km)"
+        } else { // Meter 단위
+            return "\(distance)"
+        }
     }
     
     var body: some View {
@@ -30,21 +52,13 @@ struct MapView: View {
             
             Spacer()
             
-            Button(action: {
-                //
-                print("\(distance)m 떨어져 있습니다.")
-            }, label: {
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(Color("melon"))
-                        .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: 50)
-                        .cornerRadius(10)
-                    Text("출석하기")
-                        .foregroundColor(.white)
-                        .bold()
-                }
-            })
-            .padding(.horizontal, 30)
+            if sampleLimitDis < distance {
+                notPossibleButton
+            } else {
+                possibleButton
+            }
+            
+            Spacer()
         }
         
         .edgesIgnoringSafeArea([.top, .leading, .trailing])
@@ -73,6 +87,41 @@ struct MapView: View {
                 }
             }
         }
+    }
+    
+    private var notPossibleButton: some View {
+        Button(action: {
+            // no to do
+        }, label: {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color.lightGray)
+                    .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: 50)
+                    .cornerRadius(10)
+                Text("\(distance)m 떨어져 있습니다.")
+                    .foregroundColor(.white)
+                    .bold()
+            }
+        })
+        .disabled(true)
+        .padding(.horizontal, 30)
+    }
+    
+    private var possibleButton: some View {
+        Button(action: {
+            // 출석하기 버튼 동작 수행
+        }, label: {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color("melon"))
+                    .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: 50)
+                    .cornerRadius(10)
+                Text("출석하기")
+                    .foregroundColor(.white)
+                    .bold()
+            }
+        })
+        .padding(.horizontal, 30)
     }
 }
 
