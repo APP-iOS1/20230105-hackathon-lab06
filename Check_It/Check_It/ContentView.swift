7a5da5a9a037767bd403aa8c42367a4f49d538de
김응관 <kwhn9486@naver.com>
2023-01-05 09:48:39 +0900

    [Add] 프로젝트 생성

---
+++ Check_It/Check_It/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Check_It
+//
+//  Created by 김응관 on 2023/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
