a26626724b9c170ffbe75da86ea25caca6e10bba
hwangyeri <dpfl420@naver.com>
2023-01-06 00:23:17 +0900

    mypageView 생성

--- Check_It/Check_It/View/My/MyMainView.swift
+++ Check_It/Check_It/View/My/MyMainView.swift
@@ -9,9 +9,117 @@ import SwiftUI
 
 struct MyMainView: View {
     var body: some View {
-        Text("My Main View")
+        
+        VStack {
+            HStack(alignment: .firstTextBaseline) {
+                            // MARK: - 사용자 이름 데이터 받아오기
+                            Text("반갑습니다,\n---님")
+                                .font(.largeTitle).bold()
+            
+                            Spacer()
+                            
+                            // MARK: - 프로필 사진 데이터 받아오기
+                            Image(systemName: "gearshape.fill")
+                                .resizable()
+                                .frame(width: 33, height: 33)
+                        }
+            .padding([.top, .leading, .trailing], 50)
+            
+            Spacer()
+            
+            VStack {
+                
+                Image("onejang")
+                    .resizable()
+                    .frame(width: 105, height: 105)
+                    .clipShape(Circle())
+                    .padding(.bottom, 10)
+                
+                // MARK: - 사용자 이메일 데이터 받아오기
+                Text("text@naver")
+                    .font(.body)
+                    .foregroundColor(.black)
+                
+                Button {
+                    print("dd")
+                } label: {
+                        Text("프로필 편집")
+                            .foregroundColor(.white).bold()
+                            .frame(width: 260, height: 50)
+                            .background(Color.melon)
+                            .cornerRadius(15)
+                }
+                
+            } // VStack
+            .frame(width: 300, height: 250)
+            .background(Color.lightGray)
+            .cornerRadius(15)
+            
+            Spacer()
+            
+            List {
+                
+                HStack {
+                    Button {
+                        print("dd")
+                    } label: {
+                        HStack {
+                            Text("공지사항")
+                                .font(.title3).bold()
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(.bottom)
+                
+                HStack {
+                    Button {
+                        print("dd")
+                    } label: {
+                        HStack {
+                            Text("약관 및 정책")
+                                .font(.title3).bold()
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(.bottom)
+                
+                HStack {
+                    Button {
+                        print("dd")
+                    } label: {
+                        HStack {
+                            Text("프리미엄 요금제 알아보기")
+                                .font(.title3).bold()
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(.bottom)
+                
+            }
+            .scrollContentBackground(.hidden)
+            .frame(width: 360, height: 250)
+            
+            Spacer()
+        }
+        
     }
 }
+
 
 struct MyMainView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Check_It/Check_It/View/My/UseAgreementView .swift
@@ -0,0 +1,20 @@
+//
+//  UseAgreementView .swift
+//  Check_It
+//
+//  Created by 황예리 on 2023/01/06.
+//
+
+import SwiftUI
+
+struct UseAgreementView_: View {
+    var body: some View {
+        Text("d")
+    }
+}
+
+struct UseAgreementView__Previews: PreviewProvider {
+    static var previews: some View {
+        UseAgreementView_()
+    }
+}
