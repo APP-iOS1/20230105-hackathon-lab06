868b2942b615e987a9e06692e92e0a4deaf28fa7
hwangyeri <dpfl420@naver.com>
2023-01-06 14:11:19 +0900

    [Feat] MyMainView / PremiumRateView
    ### MyMainView / PremiumRateView

--- Check_It/Check_It/View/My/premiumRateView.swift
+++ Check_It/Check_It/View/My/premiumRateView.swift
@@ -10,31 +10,42 @@ import SwiftUI
 struct premiumRateView: View {
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading, spacing: 15) {
             Spacer()
             
-            VStack(alignment: .center, spacing: 20) {
-                Text("Premium 요금제")
-                    .font(.largeTitle).bold()
+            VStack(alignment: .center, spacing: 15) {
+                Image("CheckItLogo")
+                    .resizable()
+                    .frame(width: 280, height: 80)
+                    .padding()
                 
-                HStack {
-                    Image(systemName: "wonsign")
-                        .font(.title2)
-                        .fontWeight(.semibold)
+                HStack(spacing: 25) {
+                    Text("Premium 요금제")
+                        .font(.title2).bold()
+
                     
-                    Text("3,900 / 월")
-                        .font(.title).bold()
+                    HStack(spacing: 1) {
+                        Image(systemName: "wonsign")
+                            .fontWeight(.semibold)
+                            .font(.body)
+                        
+                        
+                        Text("3,900 / 월")
+                            .font(.title2).bold()
+                    }
+                    .foregroundColor(.myRed)
                 }
-                .foregroundColor(.myRed)
                 
-                Text("동아리 최대 인원 수의 제한 해제,\n광고 제거, Custom QR Code,\nCustom MapPin, 등의 서비스를 제공합니다.")
-                    .font(.title2)
+                
+                Text("최대 인원 수의 제한 해제,광고 제거, Custom QR Code,Custom MapPin, 등의 서비스를 제공합니다.")
+                    .font(.body)
             }
-            .padding(5)
+            
+            Spacer()
             
             VStack(alignment: .leading, spacing: 5) {
                 Text("최대 인원 8명")
-                    .font(.title3)
+                    .font(.body)
                     .foregroundColor(.mediumGray)
                 
                 HStack {
@@ -44,13 +55,13 @@ struct premiumRateView: View {
                     Text("최대 인원 무제한")
                         .foregroundColor(.melon)
                 }
-                .font(.title).bold()
+                .font(.title2).bold()
                 
             }
             
             VStack(alignment: .leading, spacing: 5) {
                 Text("광고 포함")
-                    .font(.title3)
+                    .font(.body)
                 .foregroundColor(.mediumGray)
                 
                 HStack {
@@ -61,13 +72,13 @@ struct premiumRateView: View {
                     Text("광고 제거")
                         .foregroundColor(.melon)
                 }
-                .font(.title).bold()
+                .font(.title2).bold()
             }
             
             VStack(alignment: .leading, spacing: 5) {
                 
-                Text("Basic QR Code 제공")
-                    .font(.title3)
+                Text("Basic QR Code")
+                    .font(.body)
                     .foregroundColor(.mediumGray)
                 
                 HStack {
@@ -75,16 +86,16 @@ struct premiumRateView: View {
                         .foregroundColor(.myYellow)
                     
                     
-                    Text("Custom QR Code")
+                    Text("Custom QR Code 제공")
                         .foregroundColor(.melon)
                 }
-                .font(.title).bold()
+                .font(.title2).bold()
             }
             
             VStack(alignment: .leading, spacing: 5) {
                 
-                Text("Basic MapPin 제공")
-                    .font(.title3)
+                Text("Basic MapPin")
+                    .font(.body)
                     .foregroundColor(.mediumGray)
 
                 HStack {
@@ -92,11 +103,13 @@ struct premiumRateView: View {
                         .foregroundColor(.myYellow)
 
                     
-                    Text("Custom MapPin")
+                    Text("Custom MapPin 제공")
                         .foregroundColor(.melon)
                 }
-                .font(.title).bold()
+                .font(.title2).bold()
             }
+            
+            Spacer()
             
             Button(action: {}){
                 RoundedRectangle(cornerRadius: 10)
@@ -108,8 +121,10 @@ struct premiumRateView: View {
                             .foregroundColor(.white)
                             .bold()
                     }
-            }.padding(.vertical, 25)
+            }
+//            .padding(.vertical, 25)
 
+            Spacer()
         }
         .padding(.horizontal, 35)
     }
