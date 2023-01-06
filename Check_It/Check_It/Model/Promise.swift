69a257e9d8cc87f2900bb7a45909b90b8c932988
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-06 09:19:07 +0900

    [Refactor] Promise 구조 변경

--- Check_It/Check_It/Model/Promise.swift
+++ Check_It/Check_It/Model/Promise.swift
@@ -12,8 +12,8 @@ struct Promise: Identifiable, Hashable, Codable {
     var promiseName: String // 약속이름
     var limit: String// 결석 구분(30분)
     var lateLimit: String //지각 구분(5분)
-//    var rangeLimit: Int // 출석확인 가능 거리범위
-//    var location: Location // 위치
+    var rangeLimit: Int // 출석확인 가능 거리범위
+    var location: Location // 위치
     var date: String // 날짜 -> 문자열 형식으로 등록
     var startTime: String // 약속 시작시간
     var endTime: String // 약속 종료시간
