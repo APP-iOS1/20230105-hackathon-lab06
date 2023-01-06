725ff2f0a8d6bc530cd81cd886c4b0c3f6cf905b
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-06 09:20:02 +0900

    [Refactor] 나머지 코드 수정

--- Check_It/Check_It/View/Home/MapView.swift
+++ Check_It/Check_It/View/Home/MapView.swift
@@ -12,13 +12,16 @@ struct MapView: View {
     @EnvironmentObject var locationViewModel: LocationViewModel
     
     @State private var isQrcode: Bool = false // 바코드 유무
+    // promisse안에 location id들이 존재한다.
+    // 그래서 promisse location을 fetch를 해야함
+//    var promisse: Promise = Promise(promiseName: "혀미니의 또구",
+//                                    limit: "30",
+//                                    lateLimit: "5",
+//                                    date: "1월6일",
+//                                    startTime: "오후 1시",
+//                                    endTime: "오후 5시")
     
-    var promisse: Promise = Promise(promiseName:,
-                                    limit: <#T##String#>,
-                                    lateLimit: <#T##String#>,
-                                    date: <#T##String#>,
-                                    startTime: <#T##String#>,
-                                    endTime: <#T##String#>)
+//    var locationData: Location = Location(id: <#T##arg#>, locationName: <#T##String#>, address: <#T##String#>, detailAddress: <#T##String#>, geoPoint: <#T##Coordinates#>)
     
     // 임시로 만든 위치(실제로는 모임장소의 위도 경도가 들어감)
     var sampleLocation: CLLocationCoordinate2D =  CLLocationCoordinate2D(latitude: 37.478846, longitude: 126.620930)
